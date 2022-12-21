ea25fb9867bf57f104a9aacb220faf9b1adef56f
Giovane <giovanedaniell@gmail.com>
2022-12-21 13:40:29 -0300

    [client] style(components): create Wrapper variants of Logo component

--- client/src/components/Logo/styles.ts
+++ client/src/components/Logo/styles.ts
@@ -2,8 +2,20 @@ import styled, { css } from 'styled-components'
 
 import { LogoProps } from '.'
 
+const wrapperVariants = {
+  large: css`
+    width: 20rem;
+    height: 5.9rem;
+  `,
+  normal: css`
+    width: 11rem;
+    height: 3.3rem;
+  `
+}
+
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color }) => css`
-    color: ${theme.colors[color ?? 'white']};
+  ${({ theme, color, size }) => css`
+    color: ${theme.colors[color!]};
+    ${wrapperVariants[size!]};
   `}
 `
