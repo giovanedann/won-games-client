3683e0283d6e1e7805d6c41dcedf9b667233af74
Giovane <giovanedaniell@gmail.com>
2022-12-21 13:42:26 -0300

    [client] tests: create tests for size variants of Logo component

--- client/src/components/Logo/test.spec.tsx
+++ client/src/components/Logo/test.spec.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import renderWithTheme from 'utils/tests/renderWithTheme'
 import Logo from '.'
 import theme from 'styles/theme'
@@ -14,5 +14,21 @@ describe('<Logo />', () => {
     renderWithTheme(<Logo color="black" />)
     const svgWrapper = screen.getByLabelText(/won games/i).parentElement
     expect(svgWrapper).toHaveStyle({ color: theme.colors.black })
+  })
+
+  it('should render a normal logo by default', () => {
+    renderWithTheme(<Logo />)
+    const svgWrapper = screen.getByLabelText(/won games/i).parentElement
+    expect(svgWrapper).toHaveStyle({ width: '11rem', height: '3.3rem' })
+  })
+
+  it('should have the right size variants styles', () => {
+    const { rerender } = renderWithTheme(<Logo size="large" />)
+    let svgWrapper = screen.getByLabelText(/won games/i).parentElement
+    expect(svgWrapper).toHaveStyle({ width: '20rem', height: '5.9rem' })
+
+    rerender(<Logo size="normal" />)
+    svgWrapper = screen.getByLabelText(/won games/i).parentElement
+    expect(svgWrapper).toHaveStyle({ width: '11rem', height: '3.3rem' })
   })
 })
