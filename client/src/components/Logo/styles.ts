8b9ae6e5f03e63dce7f41cdbea9a382da67dbc10
Giovane <giovanedaniell@gmail.com>
2022-12-21 14:08:20 -0300

    [client] style(components): create hidden variant on Wrapper of Logo

--- client/src/components/Logo/styles.ts
+++ client/src/components/Logo/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 import { LogoProps } from '.'
 
@@ -10,12 +11,28 @@ const wrapperVariants = {
   normal: css`
     width: 11rem;
     height: 3.3rem;
+  `,
+  hidden: css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
     ${wrapperVariants[size!]};
+    ${!!hideOnMobile && wrapperVariants.hidden};
   `}
 `
