26399dbe099cfb87b95dbca4cc206ef0483148fa
Giovane <giovanedaniell@gmail.com>
2022-12-21 12:55:28 -0300

    [client] test: color style tests of Logo component

--- client/src/components/Logo/test.spec.tsx
+++ client/src/components/Logo/test.spec.tsx
@@ -1,6 +1,18 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import renderWithTheme from 'utils/tests/renderWithTheme'
 import Logo from '.'
+import theme from 'styles/theme'
 
 describe('<Logo />', () => {
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
+    const svgWrapper = screen.getByLabelText(/won games/i).parentElement
+    expect(svgWrapper).toHaveStyle({ color: theme.colors.white })
+  })
 
+  it('should render a black label when prop color is black', () => {
+    renderWithTheme(<Logo color="black" />)
+    const svgWrapper = screen.getByLabelText(/won games/i).parentElement
+    expect(svgWrapper).toHaveStyle({ color: theme.colors.black })
+  })
 })
