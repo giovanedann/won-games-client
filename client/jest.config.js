76cc6ccaf04b0fb1aef40a0be655ca9a15ede932
Giovane <giovanedaniell@gmail.com>
2022-12-21 12:42:44 -0300

    [client] chore(config): add config for absolute paths on jest

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,7 @@
 const config = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  moduleDirectories: ['node_modules', 'src'],
   collectCoverage: true,
   collectCoverageFrom: ['/src/**/*.ts(x)?', 'src/**/!stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
