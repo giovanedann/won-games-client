68d72c292631d83dae9f40ee1edd3e532fd53e90
Giovane <giovanedaniell@gmail.com>
2022-12-22 09:53:14 -0300

    [client] tests(components): create tests for MediaMatch component

--- client/src/components/MediaMatch/test.spec.tsx
+++ client/src/components/MediaMatch/test.spec.tsx
@@ -2,5 +2,37 @@ import { render, screen } from '@testing-library/react'
 import MediaMatch from '.'
 
 describe('<MediaMatch />', () => {
+  let desktopHeading: Element
+  let mobileHeading: Element
 
+  beforeEach(() => {
+    render(
+      <>
+        <MediaMatch greaterThan="medium">
+          <h1>Desktop Heading</h1>
+        </MediaMatch>
+        <MediaMatch lessThan="medium">
+          <h1>Mobile Heading</h1>
+        </MediaMatch>
+      </>
+    )
+
+    desktopHeading = screen.getByText(/desktop heading/i)
+    mobileHeading = screen.getByText(/mobile heading/i)
+  })
+
+  it('should be hidden if no media query is passed', () => {
+    expect(desktopHeading.parentElement).toHaveStyleRule('display', 'none')
+    expect(mobileHeading.parentElement).toHaveStyleRule('display', 'none')
+  })
+
+  it('should show or hide based on the media passed', () => {
+    expect(desktopHeading.parentElement).toHaveStyleRule('display', 'block', {
+      media: '(min-width: 768px)'
+    })
+
+    expect(mobileHeading.parentElement).toHaveStyleRule('display', 'block', {
+      media: '(max-width: 768px)'
+    })
+  })
 })
