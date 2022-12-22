46a7adefd85db99511b9d695a4a57b8222618299
Giovane <giovanedaniell@gmail.com>
2022-12-22 16:39:05 -0300

    [client] style(components): add styles of size variants on Button component

--- client/src/components/Button/styles.ts
+++ client/src/components/Button/styles.ts
@@ -1,3 +1,35 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { ButtonProps } from '.'
 
-export const Wrapper = styled.button``
+type WrapperProps = Pick<ButtonProps, 'size'>
+
+const wrapperVariants = {
+  small: css`
+    height: 3rem;
+    font-size: ${({ theme }) => theme.font.sizes.xsmall};
+  `,
+  medium: css`
+    height: 4rem;
+    font-size: ${({ theme }) => theme.font.sizes.small};
+    padding: ${({ theme }) =>
+      `${theme.spacings.xxsmall} ${theme.spacings.medium}`};
+  `,
+  large: css`
+    height: 5rem;
+    font-size: ${({ theme }) => theme.font.sizes.medium};
+    padding: ${({ theme }) =>
+      `${theme.spacings.xxsmall} ${theme.spacings.xlarge}`};
+  `
+}
+
+export const Wrapper = styled.button<WrapperProps>`
+  background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+  border: none;
+
+  ${({ theme, size }) => css`
+    color: ${theme.colors.white};
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
+    ${wrapperVariants[size!]};
+  `}
+`
