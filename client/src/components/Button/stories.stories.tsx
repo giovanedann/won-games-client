3414133ec21f6d97f63757e70a4a942f122ce31c
Giovane <giovanedaniell@gmail.com>
2022-12-22 17:56:16 -0300

    [client] feat(stories): add stories for buttons with icons

--- client/src/components/Button/stories.stories.tsx
+++ client/src/components/Button/stories.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { MdOutlineAddShoppingCart } from 'react-icons/md'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import Button from '.'
@@ -22,6 +23,9 @@ export default {
     },
     fullWidth: {
       type: 'boolean'
+    },
+    icon: {
+      type: 'symbol'
     }
   }
 } as ComponentMeta<typeof Button>
@@ -29,8 +33,22 @@ export default {
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
 export const Basic = Template.bind({})
+
 Basic.args = {
   children: 'Button',
   size: 'medium',
+  fullWidth: false
+}
+
+const IconTemplate: ComponentStory<typeof Button> = (args) => (
+  <Button icon={<MdOutlineAddShoppingCart />} {...args} />
+)
+
+export const WithIcon = IconTemplate.bind({})
+
+WithIcon.args = {
+  children: 'With icon',
+  size: 'medium',
+  icon: <MdOutlineAddShoppingCart />,
   fullWidth: false
 }
