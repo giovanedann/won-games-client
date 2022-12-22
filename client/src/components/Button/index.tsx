c15a8327af95dfe104a35e41dbfbee165789e311
Giovane <giovanedaniell@gmail.com>
2022-12-22 17:51:25 -0300

    [client] feat(components): add icon and onClick props to Button component

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,15 +1,29 @@
-import { ReactNode } from 'react'
+import { MouseEvent, ReactNode } from 'react'
 import * as S from './styles'
 
 export type ButtonProps = {
   children?: ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  icon?: JSX.Element
+  onClick?: () => (event: MouseEvent<HTMLButtonElement>) => void
 }
 
-function Button({ children, size = 'medium', fullWidth = false }: ButtonProps) {
+function Button({
+  children,
+  size = 'medium',
+  fullWidth = false,
+  icon,
+  onClick
+}: ButtonProps) {
   return (
-    <S.Wrapper size={size} fullWidth={fullWidth}>
+    <S.Wrapper
+      size={size}
+      fullWidth={fullWidth}
+      hasIcon={Boolean(icon)}
+      onClick={onClick}
+    >
+      {icon && icon}
       {!!children && <span>{children}</span>}
     </S.Wrapper>
   )
