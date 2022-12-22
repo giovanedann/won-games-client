f7fc59756bb243699e46f3c66ec20c123f2313f0
Giovane <giovanedaniell@gmail.com>
2022-12-22 08:24:34 -0300

    [client] feat(components): create prop color on Heading to control text color

--- client/src/components/Heading/index.tsx
+++ client/src/components/Heading/index.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from 'react'
 import * as S from './styles'
 
-type HeadingProps = {
+export type HeadingProps = {
   children: ReactNode
+  color: 'white' | 'black'
 }
 
-function Heading({ children }: HeadingProps) {
-  return <S.Wrapper>{children}</S.Wrapper>
+function Heading({ children, color = 'white' }: HeadingProps) {
+  return <S.Wrapper color={color}>{children}</S.Wrapper>
 }
 
 export default Heading
