fd741a3bf8af466657f8fe93276167ff78eaa601
Giovane <giovanedaniell@gmail.com>
2022-12-22 17:51:48 -0300

    [client] style(components): create variant styles for buttons with icons

--- client/src/components/Button/styles.ts
+++ client/src/components/Button/styles.ts
@@ -1,7 +1,9 @@
 import styled, { css } from 'styled-components'
 import { ButtonProps } from '.'
 
-type WrapperProps = Omit<ButtonProps, 'children'>
+type WrapperProps = Omit<ButtonProps, 'children'> & {
+  hasIcon: boolean
+}
 
 const wrapperVariants = {
   small: css`
@@ -22,6 +24,41 @@ const wrapperVariants = {
   `,
   fullWidth: css`
     width: 100%;
+  `,
+  hasIcon: (size: 'small' | 'medium' | 'large') => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      & + span {
+        margin-left: ${({ theme }) => theme.spacings.xxsmall};
+      }
+    }
+
+    ${size === 'small' &&
+    css`
+      svg {
+        width: 1.5rem;
+        height: 1.5rem;
+      }
+    `}
+
+    ${size === 'medium' &&
+    css`
+      svg {
+        width: 1.7rem;
+        height: 1.7rem;
+      }
+    `}
+
+    ${size === 'large' &&
+    css`
+      svg {
+        width: 2rem;
+        height: 2rem;
+      }
+    `}
   `
 }
 
@@ -31,11 +68,12 @@ export const Wrapper = styled.button<WrapperProps>`
   font-weight: 600;
   cursor: pointer;
 
-  ${({ theme, size, fullWidth }) => css`
+  ${({ theme, size, fullWidth, hasIcon }) => css`
     color: ${theme.colors.white};
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
     ${wrapperVariants[size!]};
     ${fullWidth && wrapperVariants.fullWidth};
+    ${hasIcon && wrapperVariants.hasIcon(size!)};
   `}
 `
