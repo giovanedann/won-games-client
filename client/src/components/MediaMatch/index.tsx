b84798a9dc3f2031db59af71ba26ce3aa3a00c4b
Giovane <giovanedaniell@gmail.com>
2022-12-22 09:52:09 -0300

    [client] feat(components): create variants for MediaMatch component

--- client/src/components/MediaMatch/index.tsx
+++ client/src/components/MediaMatch/index.tsx
@@ -1,11 +1,32 @@
-import * as S from './styles'
+import styled, { css } from 'styled-components'
+import media, { DefaultBreakpoints } from 'styled-media-query'
 
-function MediaMatch() {
-  return (
-    <S.Wrapper>
-      <h1>MediaMatch</h1>
-    </S.Wrapper>
-  )
+type Breakpoints = keyof DefaultBreakpoints
+
+export type MediaMatchProps = {
+  lessThan?: Breakpoints
+  greaterThan?: Breakpoints
 }
 
-export default MediaMatch
+const mediaMatchVariants = {
+  lessThan: (size: Breakpoints) => css`
+    ${media.lessThan(size)`
+      display: block;
+    `}
+  `,
+
+  greaterThan: (size: Breakpoints) => css`
+    ${media.greaterThan(size)`
+      display: block;
+    `}
+  `
+}
+
+export default styled.div<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+
+    ${lessThan && mediaMatchVariants.lessThan(lessThan)}
+    ${greaterThan && mediaMatchVariants.greaterThan(greaterThan)}
+  `}
+`
