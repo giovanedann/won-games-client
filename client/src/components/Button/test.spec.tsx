2488a63653b65d72b5b99d7b604128a27daaae50
Giovane <giovanedaniell@gmail.com>
2022-12-22 17:02:50 -0300

    [client] test: creat test for fuillWidth prop on Button component

--- client/src/components/Button/test.spec.tsx
+++ client/src/components/Button/test.spec.tsx
@@ -1,6 +1,44 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import renderWithTheme from 'utils/tests/renderWithTheme'
+import theme from 'styles/theme'
 import Button from '.'
 
 describe('<Button />', () => {
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>Buy now</Button>)
 
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '4rem',
+      padding: `${theme.spacings.xxsmall} ${theme.spacings.medium}`,
+      'font-size': `${theme.font.sizes.small}`
+    })
+  })
+
+  it('should render the small size', () => {
+    renderWithTheme(<Button size="small">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '3rem',
+      padding: theme.spacings.xxsmall,
+      'font-size': `${theme.font.sizes.xsmall}`
+    })
+  })
+
+  it('should render the large size', () => {
+    renderWithTheme(<Button size="large">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '5rem',
+      padding: `${theme.spacings.xxsmall} ${theme.spacings.xlarge}`,
+      'font-size': `${theme.font.sizes.medium}`
+    })
+  })
+
+  it('should have 100% width on fullWidth true', () => {
+    renderWithTheme(<Button fullWidth>Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      width: '100%'
+    })
+  })
 })
