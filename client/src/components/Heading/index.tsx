0bb5c40c132ac2fea05870e31f5a9620c66ad7d3
Giovane <giovanedaniell@gmail.com>
2022-12-22 08:44:43 -0300

    [client] feat(components): add props lineBottom and lineLeft to add a border on the Heading component

--- client/src/components/Heading/index.tsx
+++ client/src/components/Heading/index.tsx
@@ -4,10 +4,21 @@ import * as S from './styles'
 export type HeadingProps = {
   children: ReactNode
   color: 'white' | 'black'
+  lineLeft?: boolean
+  lineBottom?: boolean
 }
 
-function Heading({ children, color = 'white' }: HeadingProps) {
-  return <S.Wrapper color={color}>{children}</S.Wrapper>
+function Heading({
+  children,
+  color = 'black',
+  lineLeft = false,
+  lineBottom = false
+}: HeadingProps) {
+  return (
+    <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+      {children}
+    </S.Wrapper>
+  )
 }
 
 export default Heading
