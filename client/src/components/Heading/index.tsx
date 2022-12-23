96d1e62fcc8259f1ac2c006e1b4e200121099e38
Giovane <giovanedaniell@gmail.com>
2022-12-23 11:23:17 -0300

    [client] feat(components): add lineColor and size props to change line color and font-size

--- client/src/components/Heading/index.tsx
+++ client/src/components/Heading/index.tsx
@@ -1,21 +1,33 @@
 import { ReactNode } from 'react'
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  size?: 'small' | 'medium'
+  lineColor?: LineColors
 }
 
 function Heading({
   children,
   color = 'black',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium',
+  lineColor = 'primary'
 }: HeadingProps) {
   return (
-    <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+    <S.Wrapper
+      color={color}
+      lineLeft={lineLeft}
+      lineBottom={lineBottom}
+      lineColor={lineColor}
+      size={size}
+    >
       {children}
     </S.Wrapper>
   )
