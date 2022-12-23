b5d7d44d841d45d5ea367867e64be16d7059eba0
Giovane <giovanedaniell@gmail.com>
2022-12-23 08:40:21 -0300

    [client] styles(components): add icon elements and logo to Menu component

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -1,11 +1,25 @@
 import * as S from './styles'
 import { MdSearch, MdOutlineShoppingCart } from 'react-icons/md'
 import { HiOutlineMenuAlt1 } from 'react-icons/hi'
+import Logo from 'components/Logo'
 
 function Menu() {
   return (
     <S.Wrapper>
-      <h1>Menu</h1>
+      <S.IconWrapper>
+        <HiOutlineMenuAlt1 />
+      </S.IconWrapper>
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <MdSearch />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <MdOutlineShoppingCart />
+        </S.IconWrapper>
+      </S.MenuGroup>
     </S.Wrapper>
   )
 }
