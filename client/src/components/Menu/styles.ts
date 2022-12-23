09e09e49790081aef535f2e9e7fba971b48bcf9d
Giovane <giovanedaniell@gmail.com>
2022-12-23 08:40:02 -0300

    [client] styles(components): create first styled components for Menu

--- client/src/components/Menu/styles.ts
+++ client/src/components/Menu/styles.ts
@@ -1,3 +1,37 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.menu`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    padding: ${theme.spacings.small} 0;
+    position: relative;
+  `}
+`
+
+export const LogoWrapper = styled.div`
+  ${media.lessThan('medium')`
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  `}
+`
+
+export const MenuGroup = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-grow: 1;
+    justify-content: flex-end;
+    gap: ${theme.spacings.xsmall};
+  `}
+`
+
+export const IconWrapper = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    width: 2.4rem;
+    height: 2.4rem;
+    cursor: pointer;
+  `}
+`
