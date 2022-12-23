0018785389bd3ba9acedb2bdd9be91e40f49a9aa
Giovane <giovanedaniell@gmail.com>
2022-12-23 12:37:18 -0300

    [client] style(components): create styles for Footer

--- client/src/components/Footer/styles.ts
+++ client/src/components/Footer/styles.ts
@@ -1,3 +1,50 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import * as HeadingStyles from 'components/Heading/styles'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+export const FooterHeading = styled(HeadingStyles.Wrapper)`
+  text-transform: uppercase;
+`
+
+export const Wrapper = styled.footer``
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: ${theme.grid.gutter};
+    margin-top: ${theme.spacings.medium};
+
+    ${media.greaterThan('medium')`
+      grid-template-columns: repeat(4, 1fr);
+    `}
+  `}
+`
+
+export const Column = styled.div`
+  ${({ theme }) => css`
+    a,
+    span {
+      display: block;
+      color: ${theme.colors.gray[400]};
+      text-decoration: none;
+      margin-bottom: ${theme.spacings.xxsmall};
+      font-size: ${theme.font.sizes.small};
+      transition: all 0.3s ease-in-out;
+    }
+
+    a:hover {
+      color: ${theme.colors.gray[600]};
+    }
+  `}
+`
+
+export const Copyright = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray[400]};
+    font-size: ${theme.font.sizes.xsmall};
+    margin-top: ${theme.spacings.large};
+    margin-bottom: ${theme.spacings.medium};
+    text-align: center;
+  `}
+`
