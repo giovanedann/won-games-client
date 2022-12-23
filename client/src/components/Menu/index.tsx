53df37be4e2df85c398df06b40397fea88cdecd9
Giovane <giovanedaniell@gmail.com>
2022-12-23 09:28:05 -0300

    [client] feat(components): add open/close function to Menu

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -1,25 +1,31 @@
 import * as S from './styles'
-import { MdSearch, MdOutlineShoppingCart } from 'react-icons/md'
+import { MdSearch, MdOutlineShoppingCart, MdClose } from 'react-icons/md'
 import { HiOutlineMenuAlt1 } from 'react-icons/hi'
 import Logo from 'components/Logo'
+import { useState } from 'react'
 
 function Menu() {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
   return (
     <S.Wrapper>
-      <S.IconWrapper>
-        <HiOutlineMenuAlt1 />
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <HiOutlineMenuAlt1 aria-label="open menu" />
       </S.IconWrapper>
       <S.LogoWrapper>
         <Logo hideOnMobile />
       </S.LogoWrapper>
       <S.MenuGroup>
         <S.IconWrapper>
-          <MdSearch />
+          <MdSearch aria-label="search" />
         </S.IconWrapper>
         <S.IconWrapper>
-          <MdOutlineShoppingCart />
+          <MdOutlineShoppingCart aria-label="open shopping cart" />
         </S.IconWrapper>
       </S.MenuGroup>
+      <S.OpenedMenu aria-hidden={!isOpen} isOpen={isOpen}>
+        <MdClose aria-label="close menu" onClick={() => setIsOpen(false)} />
+      </S.OpenedMenu>
     </S.Wrapper>
   )
 }
