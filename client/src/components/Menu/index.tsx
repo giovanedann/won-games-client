de45fc72330a091427355016ecf7893e9e1d7660
Giovane <giovanedaniell@gmail.com>
2022-12-23 10:02:00 -0300

    [client] feat(components): create links of navigation bar on Menu component

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -3,6 +3,7 @@ import { MdSearch, MdOutlineShoppingCart, MdClose } from 'react-icons/md'
 import { HiOutlineMenuAlt1 } from 'react-icons/hi'
 import Logo from 'components/Logo'
 import { useState } from 'react'
+import Button from 'components/Button'
 
 function Menu() {
   const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -25,6 +26,23 @@ function Menu() {
       </S.MenuGroup>
       <S.OpenedMenu aria-hidden={!isOpen} isOpen={isOpen}>
         <MdClose aria-label="close menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+          <>
+            <S.MenuLink href="#">My account</S.MenuLink>
+            <S.MenuLink href="#">Wishlist</S.MenuLink>
+          </>
+        </S.MenuNav>
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Log in now
+          </Button>
+          <span>or</span>
+          <S.SignUp href="#" title="Sign Up">
+            Sign Up
+          </S.SignUp>
+        </S.RegisterBox>
       </S.OpenedMenu>
     </S.Wrapper>
   )
