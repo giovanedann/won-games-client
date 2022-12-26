eccafcc92a34087dba23785132ecc5ec3a5d9903
Giovane <giovanedaniell@gmail.com>
2022-12-26 16:02:09 -0300

    [client] tests(components): create first test of Highlight component

---
+++ client/src/components/Highlight/test.spec.tsx
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react'
+import renderWithTheme from 'utils/tests/renderWithTheme'
+import Highlight from '.'
+
+const props = {
+  title: 'Title',
+  subtitle: 'Subtitle',
+  buttonLabel: 'Buy now',
+  buttonLink: '/reddeadredemption2'
+}
+
+describe('<Highlight />', () => {
+  it('should render the right elements', () => {
+    renderWithTheme(<Highlight {...props} />)
+
+    expect(
+      screen.getByRole('heading', { level: 2, name: /title/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { level: 3, name: /subtitle/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
+  })
+})
