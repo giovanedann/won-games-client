2a7b93cdc543e729fb9c35371da7ecd32b2e73de
Giovane <giovanedaniell@gmail.com>
2022-12-26 14:22:17 -0300

    [client] feat(stories): add story variant for banners with ribbon

--- client/src/components/Banner/stories.stories.tsx
+++ client/src/components/Banner/stories.stories.tsx
@@ -7,7 +7,11 @@ import styled from 'styled-components'
 export default {
   title: 'Banner',
   component: Banner,
-  argTypes: {}
+  argTypes: {
+    ribbon: {
+      type: 'string'
+    }
+  }
 } as ComponentMeta<typeof Banner>
 
 const BannerWrapper = styled.div`
@@ -32,5 +36,22 @@ Basic.args = {
 }
 
 Basic.parameters = {
+  layout: 'fullscreen'
+}
+
+export const WithRibbon = Template.bind({})
+
+WithRibbon.args = {
+  img: 'https://scontent-gru2-2.xx.fbcdn.net/v/t1.6435-9/53586680_10157148115137560_7809745321596026880_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=973b4a&_nc_ohc=mu1Xy6YVknEAX-mRTNj&_nc_ht=scontent-gru2-2.xx&oh=00_AfDfBXMX0ha4lPSACdVOwZ9OZorxI7HW6X0kN9fzD3Z-3g&oe=63D13CAF',
+  title: 'Hades',
+  subtitle: '<p>Can you defeat <strong>Hades</strong> in this journey?</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: 'https://store.steampowered.com/app/1145360/Hades/',
+  ribbon: 'Game of the year',
+  ribbonColor: 'secondary',
+  ribbonSize: 'normal'
+}
+
+WithRibbon.parameters = {
   layout: 'fullscreen'
 }
