d1b93fe90eecef1ab33c445c86afb57083c0e501
Giovane <giovanedaniell@gmail.com>
2022-12-26 18:30:57 -0300

    [client] feat(stories): add Promotional template for GameCard story

--- client/src/components/GameCard/stories.stories.tsx
+++ client/src/components/GameCard/stories.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import GameCard from '.'
+import styled from 'styled-components'
 
 export default {
   title: 'GameCard',
@@ -9,14 +10,31 @@ export default {
   argTypes: {}
 } as ComponentMeta<typeof GameCard>
 
+const Wrapper = styled.div`
+  width: 30rem;
+`
+
 const Template: ComponentStory<typeof GameCard> = (args) => (
-  <GameCard {...args} />
+  <Wrapper>
+    <GameCard {...args} />
+  </Wrapper>
 )
 
 export const Basic = Template.bind({})
+
 Basic.args = {
   title: 'Devil May Cry 5',
   developer: 'Capcom',
   img: 'https://images6.alphacoders.com/926/thumb-1920-926723.jpg',
   price: 'R$ 235,00'
+}
+
+export const Promotional = Template.bind({})
+
+Promotional.args = {
+  title: 'Devil May Cry 5',
+  developer: 'Capcom',
+  img: 'https://images6.alphacoders.com/926/thumb-1920-926723.jpg',
+  price: 'R$ 235,00',
+  promotionalPrice: 'R$ 130,00'
 }
