149d6e6c4c34babc0ab4508b385b179c41c6c99b
Giovane <giovanedaniell@gmail.com>
2022-12-26 16:02:24 -0300

    [client] tests(stories): create base story of Highlight component

---
+++ client/src/components/Highlight/stories.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Highlight from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  argTypes: {}
+} as ComponentMeta<typeof Highlight>
+
+const Template: ComponentStory<typeof Highlight> = (args) => (
+  <Highlight {...args} />
+)
+
+export const Basic = Template.bind({})
+Basic.args = {
+  title: 'Red Dead Redemption 2',
+  subtitle: "Come to see John's new adventures!",
+  buttonLabel: 'Buy now',
+  buttonLink: '/reddeadredemption2'
+}
