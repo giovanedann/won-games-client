14fb2c8d56eb18de5d1d87e51ac45f16d5217668
Giovane <giovanedaniell@gmail.com>
2022-12-27 14:05:49 -0300

    [client] tests(stories):create test cases of GameCardSlider

---
+++ client/src/components/GameCardSlider/test.spec.tsx
@@ -0,0 +1,63 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import GameCardSlider from '.'
+import renderWithTheme from 'utils/tests/renderWithTheme'
+import theme from 'styles/theme'
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
+
+describe('<GameCardSlider />', () => {
+  it('should render with 4 visible items', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+
+    expect(container.getElementsByClassName('slick-active')).toHaveLength(4)
+  })
+
+  it('should render white arrows if color is white', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: theme.colors.white
+    })
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: theme.colors.white
+    })
+  })
+})
