dd20bcfcaf06d8292f07058bc74fd8ffb73fab94
Giovane <giovanedaniell@gmail.com>
2022-12-27 12:58:35 -0300

    [client] feat(components): create GameCardSlider component

---
+++ client/src/components/GameCardSlider/index.tsx
@@ -0,0 +1,62 @@
+import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import Slider, { SliderSettings } from 'components/Slider'
+
+import * as S from './styles'
+
+type GameCardSliderProps = {
+  items: GameCardProps[]
+  color?: 'white' | 'black'
+}
+
+const settings: SliderSettings = {
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2
+      }
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.2
+      }
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.1
+      }
+    }
+  ],
+  nextArrow: <MdArrowForwardIos aria-label="next games" />,
+  prevArrow: <MdArrowBackIos aria-label="previous games" />
+}
+
+function GameCardSlider({ items, color = 'black' }: GameCardSliderProps) {
+  return (
+    <S.Wrapper color={color}>
+      <Slider settings={settings}>
+        {items.map((item) => (
+          <GameCard key={item.title + item.developer} {...item} />
+        ))}
+      </Slider>
+    </S.Wrapper>
+  )
+}
+
+export default GameCardSlider
