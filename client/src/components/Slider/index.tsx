eb856a93784585ee7b48082d4ff44072d5977d1a
Giovane <giovanedaniell@gmail.com>
2022-12-27 08:38:44 -0300

    [client] feat(components): create Slider component

---
+++ client/src/components/Slider/index.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+import SlickSlider, { Settings } from 'react-slick'
+import * as S from './styles'
+
+export type SliderSettings = Settings
+
+export type SliderProps = {
+  children: ReactNode
+  settings: SliderSettings
+}
+
+function Slider({ children, settings }: SliderProps) {
+  return (
+    <S.Wrapper>
+      <SlickSlider {...settings}>{children}</SlickSlider>
+    </S.Wrapper>
+  )
+}
+
+export default Slider
