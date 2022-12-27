b96e4c0bc984e73854e4779a0927d08e82668368
Giovane <giovanedaniell@gmail.com>
2022-12-27 08:39:16 -0300

    [client] feat(stories): create stories for Slider

---
+++ client/src/components/Slider/stories.stories.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Slider from '.'
+import { Settings } from 'react-slick'
+
+const FakeSlide = styled.div`
+  background: gray;
+  width: 30rem;
+  padding: 10rem 0;
+  border: 0.1rem solid red;
+  color: white;
+  text-align: center;
+`
+
+export default {
+  title: 'Slider',
+  component: Slider
+} as ComponentMeta<typeof Slider>
+
+const Template: ComponentStory<typeof Slider> = (args) => (
+  <Slider {...args}>
+    {[1, 2, 3, 4, 5, 6].map((number) => (
+      <FakeSlide key={number}>{number}</FakeSlide>
+    ))}
+  </Slider>
+)
+
+const horizontalSettings: Settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1
+}
+
+export const Horizontal = Template.bind({})
+
+Horizontal.args = {
+  settings: { ...horizontalSettings }
+}
+
+const verticalSettings: Settings = {
+  vertical: true,
+  verticalSwiping: true,
+  dots: true,
+  infinite: false,
+  slidesToShow: 1
+}
+
+export const Vertical = Template.bind({})
+
+Vertical.args = {
+  settings: { ...verticalSettings }
+}
