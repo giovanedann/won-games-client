caa7f1b34b6298a2a58417f2e8cb707ed220c864
Giovane <giovanedaniell@gmail.com>
2022-12-27 09:02:20 -0300

    [client] feat(stories): create stories for BannerSlider

---
+++ client/src/components/BannerSlider/stories.stories.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import BannerSlider from '.'
+import styled from 'styled-components'
+
+const items = [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x581',
+    title: 'Defy death 3',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+const Wrapper = styled.div`
+  max-width: 130rem;
+  margin: 0 auto;
+`
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider
+} as ComponentMeta<typeof BannerSlider>
+
+const Template: ComponentStory<typeof BannerSlider> = (args) => (
+  <Wrapper>
+    <BannerSlider {...args} />
+  </Wrapper>
+)
+
+export const Basic = Template.bind({})
+
+Basic.args = {
+  items: items
+}
+
+Basic.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
