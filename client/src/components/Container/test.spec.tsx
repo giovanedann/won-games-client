367f88adb112351dfa19d73945f216ad87b8c9c8
Giovane <giovanedaniell@gmail.com>
2022-12-28 10:11:27 -0300

    [client] tests(components): add test cases for Container component

---
+++ client/src/components/Container/test.spec.tsx
@@ -0,0 +1,27 @@
+import theme from 'styles/theme'
+import renderWithTheme from 'utils/tests/renderWithTheme'
+import Container from '.'
+
+describe('<Container />', () => {
+  it('should render the container with the right styles', () => {
+    const { container } = renderWithTheme(<Container />)
+
+    expect(container.firstChild).toHaveStyle({
+      'max-width': theme.grid.container
+    })
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(3.2rem / 2);
+        padding-right: calc(3.2rem / 2);
+      }
+
+      <div
+        class="c0"
+      />
+    `)
+  })
+})
