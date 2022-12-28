4ad8b40b71778eca441116bd7781300bdb71dd39
Giovane <giovanedaniell@gmail.com>
2022-12-28 11:02:17 -0300

    [client] feat(templates): add Menu, Footer and content sections on Home

--- client/src/templates/Home/index.tsx
+++ client/src/templates/Home/index.tsx
@@ -1,10 +1,45 @@
+import Container from 'components/Container'
+import Footer from 'components/Footer'
+import Heading from 'components/Heading'
+import Menu from 'components/Menu'
+
 import * as S from './styles'
 
 function Home() {
   return (
-    <S.Wrapper>
-      <h1>Home</h1>
-    </S.Wrapper>
+    <section>
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <Heading lineLeft lineColor="secondary" color="black">
+          News
+        </Heading>
+      </Container>
+
+      <Container>
+        <Heading lineLeft lineColor="secondary" color="white">
+          Most popular
+        </Heading>
+      </Container>
+
+      <Container>
+        <Heading lineLeft lineColor="secondary" color="white">
+          Upcoming
+        </Heading>
+      </Container>
+
+      <Container>
+        <Heading lineLeft lineColor="secondary" color="white">
+          Free games
+        </Heading>
+      </Container>
+
+      <Container>
+        <Footer />
+      </Container>
+    </section>
   )
 }
 
