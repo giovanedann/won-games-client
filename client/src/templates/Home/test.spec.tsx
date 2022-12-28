5ed4c5a24f11d3f971eb6e7db6cea24d71765b01
Giovane <giovanedaniell@gmail.com>
2022-12-28 12:16:03 -0300

    [client] tests(templates): add home component mocks and render component on beforeEach

--- client/src/templates/Home/test.spec.tsx
+++ client/src/templates/Home/test.spec.tsx
@@ -1,11 +1,29 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import renderWithTheme from 'utils/tests/renderWithTheme'
 import Home from '.'
+import bannersMock from 'components/BannerSlider/data.mock'
+import gamesMock from 'components/GameCardSlider/data.mock'
+import highlightMock from 'components/Highlight/data.mock'
+
+const props = {
+  banners: bannersMock,
+  newGames: gamesMock,
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: gamesMock,
+  upcomingGames: gamesMock,
+  upcomingHighlight: highlightMock,
+  upcomingMoreGames: gamesMock,
+  freeGames: gamesMock,
+  freeHighlight: highlightMock
+}
 
 describe('<Home />', () => {
-  it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+  beforeEach(() => {
+    renderWithTheme(<Home {...props} />)
+  })
 
+  it('should render menu and footer', () => {
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
 
     expect(
@@ -14,8 +32,6 @@ describe('<Home />', () => {
   })
 
   it('should render the right section headings', () => {
-    renderWithTheme(<Home />)
-
     const headings = ['news', 'most popular', 'upcoming', 'free games']
 
     headings.forEach((heading) => {
@@ -24,5 +40,13 @@ describe('<Home />', () => {
         screen.getByRole('heading', { level: 2, name: headingRegExp })
       ).toBeInTheDocument()
     })
+  })
+
+  it('should render sections elements', () => {
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
+
+    expect(screen.getAllByText(/red dead redemption 2/i)).toHaveLength(3)
   })
 })
