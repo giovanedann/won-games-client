7d9f1b67c94c67cf27b7c5a3a7cc242af69043d2
Giovane <giovanedaniell@gmail.com>
2022-12-28 11:02:34 -0300

    [client] tests(templates): add test cases for section headings

--- client/src/templates/Home/test.spec.tsx
+++ client/src/templates/Home/test.spec.tsx
@@ -1,6 +1,28 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import renderWithTheme from 'utils/tests/renderWithTheme'
 import Home from '.'
 
 describe('<Home />', () => {
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home />)
 
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { level: 2, name: /contact/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the right section headings', () => {
+    renderWithTheme(<Home />)
+
+    const headings = ['news', 'most popular', 'upcoming', 'free games']
+
+    headings.forEach((heading) => {
+      const headingRegExp = new RegExp(heading, 'i')
+      expect(
+        screen.getByRole('heading', { level: 2, name: headingRegExp })
+      ).toBeInTheDocument()
+    })
+  })
 })
