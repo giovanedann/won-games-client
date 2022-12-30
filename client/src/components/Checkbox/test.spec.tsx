694ddd1a53fbd91b31e9b29d16b428ad24533f95
Giovane <giovanedaniell@gmail.com>
2022-12-30 10:11:15 -0300

    [client] tests(components): add test cases for props label and labelFor of Checkbox

--- client/src/components/Checkbox/test.spec.tsx
+++ client/src/components/Checkbox/test.spec.tsx
@@ -1,6 +1,19 @@
 import { render, screen } from '@testing-library/react'
+import renderWithTheme from 'utils/tests/renderWithTheme'
 import Checkbox from '.'
 
 describe('<Checkbox />', () => {
+  it('should render with a label', () => {
+    renderWithTheme(<Checkbox label="My label" labelFor="mycheckbox" />)
 
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+    expect(screen.getByLabelText(/my label/i)).toBeInTheDocument()
+    expect(screen.getByText(/my label/i)).toHaveAttribute('for', 'mycheckbox')
+  })
+
+  it('should render without a label', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByRole('label')).not.toBeInTheDocument()
+  })
 })
