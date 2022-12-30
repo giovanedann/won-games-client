068c685bac0812fc5336417efd6f103a2f63dd8a
Giovane <giovanedaniell@gmail.com>
2022-12-30 10:16:35 -0300

    [client] tests(components): add test cases for label color on Checkbox

--- client/src/components/Checkbox/test.spec.tsx
+++ client/src/components/Checkbox/test.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import theme from 'styles/theme'
 import renderWithTheme from 'utils/tests/renderWithTheme'
 import Checkbox from '.'
 
@@ -15,5 +16,21 @@ describe('<Checkbox />', () => {
     renderWithTheme(<Checkbox />)
 
     expect(screen.queryByRole('label')).not.toBeInTheDocument()
+  })
+
+  it('should render with a white label by default', () => {
+    renderWithTheme(<Checkbox label="My label" labelFor="mycheckbox" />)
+    expect(screen.getByText(/my label/i)).toHaveStyle({
+      color: theme.colors.white
+    })
+  })
+
+  it('should render with a black label', () => {
+    renderWithTheme(
+      <Checkbox label="My label" labelFor="mycheckbox" labelColor="black" />
+    )
+    expect(screen.getByText(/my label/i)).toHaveStyle({
+      color: theme.colors.black
+    })
   })
 })
