d7cf06e48096de472349445f09365b513f5b1a56
Giovane <giovanedaniell@gmail.com>
2022-12-30 10:43:11 -0300

    [client] feat(components): add controlled state to control the value of the Checkbox

--- client/src/components/Checkbox/index.tsx
+++ client/src/components/Checkbox/index.tsx
@@ -1,19 +1,37 @@
+import { InputHTMLAttributes, useState } from 'react'
 import * as S from './styles'
 
-export type CheckboxProps = {
+export type CheckboxProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
+  onCheck?: (value: boolean) => void
 }
 
 function Checkbox({
+  onCheck,
   label,
   labelFor = '',
   labelColor = 'white'
 }: CheckboxProps) {
+  const [isChecked, setIsChecked] = useState<boolean>(false)
+
+  function onChange() {
+    setIsChecked((prev) => !prev)
+
+    if (onCheck) {
+      onCheck(isChecked)
+    }
+  }
+
   return (
     <S.Wrapper>
-      <S.Input type="checkbox" id={labelFor} />
+      <S.Input
+        type="checkbox"
+        id={labelFor}
+        onChange={onChange}
+        checked={isChecked}
+      />
 
       {label && (
         <S.Label htmlFor={labelFor} role="label" labelColor={labelColor}>
