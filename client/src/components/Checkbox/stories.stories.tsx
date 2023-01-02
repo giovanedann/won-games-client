bd690d57d021b0d3362a28b39867f9ad901984cf
Giovane <giovanedaniell@gmail.com>
2023-01-02 20:35:38 -0300

    [client] style(stories): add more checkboxes on Checkbox story to test accessibility

--- client/src/components/Checkbox/stories.stories.tsx
+++ client/src/components/Checkbox/stories.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import Checkbox from '.'
+import styled from 'styled-components'
 
 export default {
   title: 'Checkbox',
@@ -11,20 +12,36 @@ export default {
   }
 } as ComponentMeta<typeof Checkbox>
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1.5rem;
+`
+
 const Template: ComponentStory<typeof Checkbox> = (args) => (
-  <Checkbox {...args} />
+  <Container>
+    <Checkbox
+      name="category"
+      label="Adventure"
+      labelFor="adventure"
+      isChecked
+      {...args}
+    />
+    <Checkbox name="category" label="Action" labelFor="action" {...args} />
+    <Checkbox name="category" isChecked label="RPG" labelFor="rpg" {...args} />
+  </Container>
 )
 
 export const Basic = Template.bind({})
 
 Basic.parameters = {
+  layout: 'fullscreen',
   backgrounds: {
     default: 'won-dark'
   }
 }
 
 Basic.args = {
-  label: 'Checkbox',
-  labelFor: 'mycheckbox',
   color: 'white'
 }
