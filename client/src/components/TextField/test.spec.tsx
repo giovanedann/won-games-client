aa21a49e4ba8db5622263bda02ad5f8e0f4f2ba6
Giovane <giovanedaniell@gmail.com>
2023-01-06 00:06:58 -0300

    [client] tests(components): add tests for TextField component

---
+++ client/src/components/TextField/test.spec.tsx
@@ -0,0 +1,65 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import renderWithTheme from 'utils/tests/renderWithTheme'
+import TextField from '.'
+
+describe('<TextField />', () => {
+  it('should render the label', () => {
+    renderWithTheme(<TextField label="text" labelFor="text" id="text" />)
+
+    expect(screen.getByLabelText(/text/i)).toBeInTheDocument()
+  })
+
+  it('should render without the label', () => {
+    renderWithTheme(<TextField labelFor="text" id="text" />)
+
+    expect(screen.queryByLabelText(/text/i)).not.toBeInTheDocument()
+  })
+
+  it('should render the placeholder', () => {
+    renderWithTheme(
+      <TextField labelFor="text" id="text" placeholder="jane.doe@mail.com" />
+    )
+
+    expect(
+      screen.getByPlaceholderText(/jane.doe@mail.com/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should change the value when typing', async () => {
+    const user = userEvent.setup()
+    const inputHandler = jest.fn()
+
+    renderWithTheme(
+      <TextField
+        label="text"
+        labelFor="text"
+        id="text"
+        placeholder="jane.doe@mail.com"
+        onInputChange={inputHandler}
+      />
+    )
+
+    const text = 'typing...'
+    await user.type(screen.getByRole('textbox'), text)
+
+    expect(screen.getByRole('textbox')).toHaveValue(text)
+
+    expect(inputHandler).toBeCalled()
+    expect(inputHandler).toBeCalledTimes(text.length)
+    expect(inputHandler).toBeCalledWith(text)
+  })
+
+  it('should be accessible by tab', async () => {
+    const user = userEvent.setup()
+    renderWithTheme(
+      <TextField labelFor="text" id="text" placeholder="jane.doe@mail.com" />
+    )
+
+    expect(document.body).toHaveFocus()
+
+    await user.tab()
+
+    expect(screen.getByRole('textbox')).toHaveFocus()
+  })
+})
