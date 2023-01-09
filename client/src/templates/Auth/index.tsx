ff47956fab42ef392f6876e45bd18bb62f100b01
Giovane <giovanedaniell@gmail.com>
2023-01-09 19:29:29 -0300

    [client] feat(templates): create structure components of Auth page

--- client/src/templates/Auth/index.tsx
+++ client/src/templates/Auth/index.tsx
@@ -1,9 +1,35 @@
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+import { ReactNode } from 'react'
 import * as S from './styles'
 
-function Auth() {
+type AuthProps = {
+  formTitle: string
+  children: ReactNode
+}
+
+function Auth({ children, formTitle }: AuthProps) {
   return (
     <S.Wrapper>
-      <h1>Auth</h1>
+      <S.BannerBlock>
+        <Logo />
+        <Heading>All your favorite games in one place!</Heading>
+        <S.Subtitle>
+          <strong>WON</strong> is the best and most complete gaming platform in
+          the web.
+        </S.Subtitle>
+        <S.Footer>
+          Won Games & Giovane Saes 2022 © All rights reserved.
+        </S.Footer>
+      </S.BannerBlock>
+
+      <S.Content>
+        <Logo color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {formTitle}
+        </Heading>
+        {children}
+      </S.Content>
     </S.Wrapper>
   )
 }
