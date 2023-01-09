4389dbb3ca6efaccc5e4836c866210c95b30b17d
Giovane <giovanedaniell@gmail.com>
2023-01-09 19:17:39 -0300

    [client] feat(templates): create template for Auth pages

---
+++ client/src/templates/Auth/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+function Auth() {
+  return (
+    <S.Wrapper>
+      <h1>Auth</h1>
+    </S.Wrapper>
+  )
+}
+
+export default Auth
---
+++ client/src/templates/Auth/test.spec.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react'
+import Auth from '.'
+
+describe('<Auth />', () => {
+
+})
