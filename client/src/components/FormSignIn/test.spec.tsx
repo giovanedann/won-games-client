4430b80961cf0dad9c88d7faac89abff92d0b1a0
Giovane <giovanedaniell@gmail.com>
2023-01-14 19:40:34 -0300

    [client] tests(components): add first FormSignIn tests

--- client/src/components/FormSignIn/test.spec.tsx
+++ client/src/components/FormSignIn/test.spec.tsx
@@ -1,6 +1,28 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import renderWithTheme from 'utils/tests/renderWithTheme'
 import FormSignIn from '.'
 
 describe('<FormSignIn />', () => {
+  it('should render the text inputs and sign in button', () => {
+    renderWithTheme(<FormSignIn />)
 
+    expect(screen.getByPlaceholderText(/e-mail/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /sign in/i })).toBeInTheDocument()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /forgot your password\?/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the text and link to sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByText(/don't have an account/i)).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+  })
 })
