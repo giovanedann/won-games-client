cf4b1f80d71b2188087a72787e31156124839656
Giovane <giovanedaniell@gmail.com>
2023-01-14 19:15:17 -0300

    [client] feat(components): create initial template of FormSignIn component

---
+++ client/src/components/FormSignIn/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+function FormSignIn() {
+  return (
+    <S.Wrapper>
+      <h1>FormSignIn</h1>
+    </S.Wrapper>
+  )
+}
+
+export default FormSignIn
---
+++ client/src/components/FormSignIn/stories.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import FormSignIn from '.'
+
+export default {
+  title: 'FormSignIn',
+  component: FormSignIn,
+  argTypes: {
+    // place your args types here
+  }
+} as ComponentMeta<typeof FormSignIn>
+
+const Template: ComponentStory<typeof FormSignIn> = (args) => <FormSignIn {...args} />
+
+export const Basic = Template.bind({})
+Basic.args = {} // default values for your props
