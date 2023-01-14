7a89d95813e941b220c672edb4d5b711b21758aa
Giovane <giovanedaniell@gmail.com>
2023-01-14 19:47:55 -0300

    [client] style(components): create styles of FormSignIn

--- client/src/components/FormSignIn/styles.ts
+++ client/src/components/FormSignIn/styles.ts
@@ -1,9 +1,52 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { lighten, darken } from 'polished'
 
-export const Wrapper = styled.main``
+import * as TextFieldStyles from 'components/TextField/styles'
+import * as ButtonStyles from 'components/Button/styles'
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    ${TextFieldStyles.Wrapper} {
+      margin: ${theme.spacings.xxsmall} 0;
+    }
+    ${ButtonStyles.Wrapper} {
+      margin: ${theme.spacings.medium} auto ${theme.spacings.xsmall};
+    }
+  `}
+`
 
 export const Form = styled.form``
 
-export const ForgotPassword = styled.a``
+export const ForgotPassword = styled.a`
+  ${({ theme }) => css`
+    display: block;
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-decoration: none;
+    text-align: right;
 
-export const FormLink = styled.div``
+    &:hover {
+      color: ${lighten(0.2, theme.colors.black)};
+    }
+  `}
+`
+
+export const FormLink = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-align: center;
+
+    a {
+      color: ${theme.colors.secondary};
+      text-decoration: none;
+      border-bottom: 0.1rem solid ${theme.colors.secondary};
+      transition: color, border, ${theme.transitions.fast};
+
+      &:hover {
+        color: ${darken(0.1, theme.colors.secondary)};
+        border-bottom: 0.1rem solid ${darken(0.1, theme.colors.secondary)};
+      }
+    }
+  `}
+`
