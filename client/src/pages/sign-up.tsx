89df5452b33e9a90d4619d28ca1df60512313cc3
Giovane <giovanedaniell@gmail.com>
2023-01-15 00:27:36 -0300

    [client] feat(pages): add forms to sign in and sign up pages

--- client/src/pages/sign-up.tsx
+++ client/src/pages/sign-up.tsx
@@ -1,5 +1,10 @@
+import FormSignUp from 'components/FormSignUp'
 import Auth from 'templates/Auth'
 
 export default function SignIn() {
-  return <Auth formTitle="Sign up">Sign up</Auth>
+  return (
+    <Auth formTitle="Sign up">
+      <FormSignUp />
+    </Auth>
+  )
 }
