0719236a9706a0177c57265278b4a91d8e08292c
Giovane <giovanedaniell@gmail.com>
2023-01-28 13:50:05 -0300

    feat(stories): add stories for Showcase component

--- src/components/Showcase/stories.stories.tsx
+++ src/components/Showcase/stories.stories.tsx
@@ -1,17 +1,56 @@
 import React from 'react'
+import styled from 'styled-components'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import Showcase from '.'
+import gamesMock from 'components/GameCardSlider/data.mock'
+import highlightMock from 'components/Highlight/data.mock'
+
+const Container = styled.div`
+  margin: 0 auto;
+`
 
 export default {
   title: 'Showcase',
   component: Showcase,
-  argTypes: {
-    // place your args types here
-  }
+  decorators: [
+    (Story) => (
+      <Container>
+        <Story />
+      </Container>
+    )
+  ]
 } as ComponentMeta<typeof Showcase>
 
-const Template: ComponentStory<typeof Showcase> = (args) => <Showcase {...args} />
+const Template: ComponentStory<typeof Showcase> = (args) => (
+  <Showcase {...args} />
+)
+
+Template.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'won-dark'
+  }
+}
 
 export const Basic = Template.bind({})
-Basic.args = {} // default values for your props
+
+Basic.args = {
+  title: 'Most popular',
+  games: gamesMock,
+  highlight: highlightMock
+}
+
+export const WithoutHighlight = Template.bind({})
+
+WithoutHighlight.args = {
+  title: 'Most popular',
+  games: gamesMock
+}
+
+export const WithoutGames = Template.bind({})
+
+WithoutGames.args = {
+  title: 'Most popular',
+  highlight: highlightMock
+}
