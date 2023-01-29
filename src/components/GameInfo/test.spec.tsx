cc0bf9f266afb56424720d1957031519408bb75d
Giovane <giovanedaniell@gmail.com>
2023-01-29 16:15:52 -0300

    test(components): add first tests of GameInfo

--- src/components/GameInfo/test.spec.tsx
+++ src/components/GameInfo/test.spec.tsx
@@ -1,6 +1,30 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import renderWithTheme from 'utils/tests/renderWithTheme'
 import GameInfo from '.'
 
-describe('<GameInfo />', () => {
+const props = {
+  title: 'Hades',
+  description: 'Hades description',
+  price: '210,00'
+}
 
+describe('<GameInfo />', () => {
+  it('should render the game info', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    expect(screen.getByRole('heading', { name: /hades/i })).toBeInTheDocument()
+    expect(screen.getByText(/hades description/i)).toBeInTheDocument()
+    expect(screen.getByText(/\$210,00/i)).toBeInTheDocument()
+  })
+
+  it('should render the action buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('button', { name: /wishlist/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+  })
 })
