01dbf54e42941e98c7c5eb3860ae917177502de0
Giovane <giovanedaniell@gmail.com>
2023-01-29 16:27:17 -0300

    feat(stories): add GameInfo story

--- src/components/GameInfo/stories.stories.tsx
+++ src/components/GameInfo/stories.stories.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import GameInfo from '.'
+import gameInfoMock from './data.mock'
 
 export default {
   title: 'GameInfo',
-  component: GameInfo,
-  argTypes: {
-    // place your args types here
-  }
+  component: GameInfo
 } as ComponentMeta<typeof GameInfo>
 
-const Template: ComponentStory<typeof GameInfo> = (args) => <GameInfo {...args} />
+const Template: ComponentStory<typeof GameInfo> = (args) => (
+  <GameInfo {...args} />
+)
 
 export const Basic = Template.bind({})
-Basic.args = {} // default values for your props
+
+Basic.parameters = {
+  backgrounds: {
+    default: 'won-dark'
+  }
+}
+
+Basic.args = {
+  ...gameInfoMock
+}
