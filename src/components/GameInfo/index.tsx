75c6a1c0cc323275a048b6d7c19a39b0836a27eb
Giovane <giovanedaniell@gmail.com>
2023-01-29 16:15:35 -0300

    feat(components): add initial structure of GameInfo

--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -1,9 +1,35 @@
+import { MdOutlineAddShoppingCart, MdFavoriteBorder } from 'react-icons/md'
 import * as S from './styles'
 
-function GameInfo() {
+import Button from 'components/Button'
+import Heading from 'components/Heading'
+import Ribbon from 'components/Ribbon'
+
+export type GameInfoProps = {
+  title: string
+  description: string
+  price: string
+}
+
+function GameInfo({ description, price, title }: GameInfoProps) {
   return (
     <S.Wrapper>
-      <h1>GameInfo</h1>
+      <Heading color="black" lineBottom>
+        {title}
+      </Heading>
+
+      <Ribbon color="secondary">{`$${price}`}</Ribbon>
+
+      <S.Description>{description}</S.Description>
+
+      <S.ButtonsWrapper>
+        <Button icon={<MdFavoriteBorder />} size="large" minimal>
+          Wishlist
+        </Button>
+        <Button icon={<MdOutlineAddShoppingCart />} size="large">
+          Add to cart
+        </Button>
+      </S.ButtonsWrapper>
     </S.Wrapper>
   )
 }
