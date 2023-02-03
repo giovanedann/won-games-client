5bf6b61bc3a0c6c6797f7e2bae8f106ea33cc64e
Giovane <giovanedaniell@gmail.com>
2023-02-02 22:22:29 -0300

    test: create first tests of Gallery component

---
+++ src/components/Gallery/test.spec.tsx
@@ -0,0 +1,51 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import renderWithTheme from 'utils/tests/renderWithTheme'
+import Gallery from '.'
+import userEvent from '@testing-library/user-event'
+import galleryMock from './data.mock'
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  default: (props: any) => {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img role="button" {...props} />
+  }
+}))
+
+describe('<Gallery />', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+    jest.restoreAllMocks()
+  })
+
+  it('should render the thumbnails as buttons', async () => {
+    renderWithTheme(<Gallery items={galleryMock.slice(0, 2)} />)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    ).toHaveAttribute('src', galleryMock[0].src)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
+    ).toHaveAttribute('src', galleryMock[1].src)
+  })
+
+  it('should handle open modal', async () => {
+    renderWithTheme(<Gallery items={galleryMock.slice(0, 2)} />)
+
+    const modal = screen.getByLabelText('modal')
+    const user = userEvent.setup()
+
+    expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
+    expect(modal).toHaveAttribute('aria-hidden', 'true')
+
+    await user.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+
+    expect(modal).toHaveAttribute('aria-hidden', 'false')
+    expect(modal).toHaveStyle({ opacity: 1 })
+  })
+})
