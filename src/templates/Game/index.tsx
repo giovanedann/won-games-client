cbc57be77ae491412d2277fae98e6e74d80be993
Giovane <giovanedaniell@gmail.com>
2023-02-04 17:23:19 -0300

    fix(typo): change upcomming to upcoming

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -15,8 +15,8 @@ export type GameTemplateProps = {
   gallery?: GalleryImageProps[]
   description: string
   details: GameDetailsProps
-  upcommingGames: GameCardProps[]
-  upcommingHighlight: HighlightProps
+  upcomingGames: GameCardProps[]
+  upcomingHighlight: HighlightProps
   recommendedGames: GameCardProps[]
 }
 
@@ -27,8 +27,8 @@ function Game({
   description,
   details,
   recommendedGames,
-  upcommingGames,
-  upcommingHighlight
+  upcomingGames,
+  upcomingHighlight
 }: GameTemplateProps) {
   return (
     <Base>
@@ -55,9 +55,9 @@ function Game({
       </S.Main>
 
       <Showcase
-        title="Upcomming"
-        games={upcommingGames}
-        highlight={upcommingHighlight}
+        title="Upcoming"
+        games={upcomingGames}
+        highlight={upcomingHighlight}
       />
 
       <Showcase title="You may like these games" games={recommendedGames} />
