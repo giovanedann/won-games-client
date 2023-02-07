8261845f757f2b76da26fe183507edf131ee5bb6
Giovane <giovanedaniell@gmail.com>
2023-02-06 23:40:38 -0300

    feat(components): add prop to ProfileMenu to control the styles of the selected items

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -7,20 +7,36 @@ import {
 
 import * as S from './styles'
 
-function ProfileMenu() {
+export type ProfileMenuProps = {
+  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders'
+}
+
+function ProfileMenu({ activeLink }: ProfileMenuProps) {
   return (
     <S.Nav>
-      <S.StyledLink href="/profile/me">
+      <S.StyledLink
+        href="/profile/me"
+        isActive={activeLink === '/profile/me'}
+        title="my profile"
+      >
         <MdAccountCircle size={24} />
         <span>My profile</span>
       </S.StyledLink>
 
-      <S.StyledLink href="/profile/cards">
+      <S.StyledLink
+        href="/profile/cards"
+        isActive={activeLink === '/profile/cards'}
+        title="my cards"
+      >
         <MdCreditCard size={24} />
         <span>My cards</span>
       </S.StyledLink>
 
-      <S.StyledLink href="/profile/orders">
+      <S.StyledLink
+        href="/profile/orders"
+        isActive={activeLink === '/profile/orders'}
+        title="my orders"
+      >
         <MdFormatListBulleted size={24} />
         <span>My orders</span>
       </S.StyledLink>
