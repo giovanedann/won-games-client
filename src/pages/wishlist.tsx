1c39baa71b9229d4be50560d0366438bb59d3fb1
Giovane <giovanedaniell@gmail.com>
2023-02-06 21:30:12 -0300

    feat(page): add mocked data to wishlist page

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,5 +1,17 @@
-import WishlistTemplate from 'templates/Wishlist'
+import WishlistTemplate, { WishlistTemplateProps } from 'templates/Wishlist'
 
-export default function Wishlist() {
-  return <WishlistTemplate />
+import gamesMock from 'components/GameCardSlider/data.mock'
+import highlightMock from 'components/Highlight/data.mock'
+
+export default function Wishlist(props: WishlistTemplateProps) {
+  return <WishlistTemplate {...props} />
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      recommendedGames: gamesMock,
+      recommendedHighlight: highlightMock
+    }
+  }
 }
