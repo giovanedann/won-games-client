314f90c3ef2f4236e86fe775e9676264e7985930
Giovane <giovanedaniell@gmail.com>
2023-02-09 22:43:14 -0300

    feat(stories): add paymentInfo to WithPayment story template

--- src/components/GameItem/stories.stories.tsx
+++ src/components/GameItem/stories.stories.tsx
@@ -29,5 +29,11 @@ WithPayment.args = {
   img: 'https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png',
   price: 'R$ 225,50',
   title: 'God of War - Ragnarok',
-  downloadLink: 'https://wongames.com/download/god-of-war-ragnarok'
+  downloadLink: 'https://wongames.com/download/god-of-war-ragnarok',
+  paymentInfo: {
+    flag: 'mastercard',
+    img: 'img/credit-cards/master-card.png',
+    number: '**** **** **** 4326',
+    purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
+  }
 }
