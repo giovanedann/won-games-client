555976eedc6712576b687aafcfc792f4512c806b
Giovane <giovanedaniell@gmail.com>
2023-02-09 22:05:13 -0300

    test(components): add first tests to GameItem

---
+++ src/components/GameItem/test.spec.tsx
@@ -0,0 +1,40 @@
+import { screen } from '@testing-library/react'
+import renderWithTheme from 'utils/tests/renderWithTheme'
+import GameItem from '.'
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  default: (props: any) => {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img {...props} />
+  }
+}))
+
+const props = {
+  img: 'image-source',
+  title: 'Red Dead Redemption 2',
+  price: 'R$ 215,00'
+}
+
+describe('<GameItem />', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+    jest.restoreAllMocks()
+  })
+
+  it('should render the item', () => {
+    renderWithTheme(<GameItem {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByText('R$ 215,00')).toBeInTheDocument()
+  })
+})
