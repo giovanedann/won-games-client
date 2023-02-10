34c7696c83ed18c4025c62dc51879867edf4c375
Giovane <giovanedaniell@gmail.com>
2023-02-09 23:43:16 -0300

    feat: add strategy to disable PaymentOptions buy button

--- src/components/PaymentOptions/index.tsx
+++ src/components/PaymentOptions/index.tsx
@@ -5,6 +5,7 @@ import Button from 'components/Button'
 import Heading from 'components/Heading'
 import Radio from 'components/Radio'
 import * as S from './styles'
+import { useState } from 'react'
 
 export type PaymentOptionsProps = {
   cards?: PaymentCard[]
@@ -17,47 +18,56 @@ export type PaymentCard = {
   img: string
 }
 
-const PaymentOptions = ({ cards, handlePayment }: PaymentOptionsProps) => (
-  <S.Wrapper>
-    <S.Body>
-      <Heading color="black" size="small" lineBottom>
-        Payment
-      </Heading>
+const PaymentOptions = ({ cards, handlePayment }: PaymentOptionsProps) => {
+  const [isChecked, setIsChecked] = useState(false)
 
-      <S.CardsList>
-        {cards?.map((card) => (
-          <S.CardItem key={card.number}>
-            <S.CardInfo>
-              <S.CardImageBox>
-                <Image src={card.img} alt={card.flag} fill />
-              </S.CardImageBox>
-              {card.number}
-            </S.CardInfo>
+  return (
+    <S.Wrapper>
+      <S.Body>
+        <Heading color="black" size="small" lineBottom>
+          Payment
+        </Heading>
 
-            <Radio
-              name="credit-card"
-              id={card.number}
-              value={card.number}
-              onCheck={() => ({})}
-            />
-          </S.CardItem>
-        ))}
+        <S.CardsList>
+          {cards?.map((card) => (
+            <S.CardItem key={card.number}>
+              <S.CardInfo>
+                <S.CardImageBox>
+                  <Image src={card.img} alt={card.flag} fill />
+                </S.CardImageBox>
+                <S.RadioLabel htmlFor={card.number}>{card.number}</S.RadioLabel>
+              </S.CardInfo>
 
-        <S.AddCard role="button">
-          <MdAdd size={14} /> Add a new credit card
-        </S.AddCard>
-      </S.CardsList>
-    </S.Body>
+              <Radio
+                name="credit-card"
+                id={card.number}
+                value={card.number}
+                onCheck={() => setIsChecked(true)}
+              />
+            </S.CardItem>
+          ))}
 
-    <S.Footer>
-      <Button as="a" fullWidth minimal>
-        Continue shopping
-      </Button>
-      <Button fullWidth icon={<MdShoppingCart />} onClick={handlePayment}>
-        Buy now
-      </Button>
-    </S.Footer>
-  </S.Wrapper>
-)
+          <S.AddCard role="button">
+            <MdAdd size={14} /> Add a new credit card
+          </S.AddCard>
+        </S.CardsList>
+      </S.Body>
+
+      <S.Footer>
+        <Button as="a" fullWidth minimal>
+          Continue shopping
+        </Button>
+        <Button
+          fullWidth
+          icon={<MdShoppingCart />}
+          onClick={handlePayment}
+          disabled={!isChecked}
+        >
+          Buy now
+        </Button>
+      </S.Footer>
+    </S.Wrapper>
+  )
+}
 
 export default PaymentOptions
