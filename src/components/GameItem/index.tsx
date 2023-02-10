e39fdc37821e8081f8be84ab3e8808b172d7ffac
Giovane <giovanedaniell@gmail.com>
2023-02-09 22:42:06 -0300

    feat(components): add paymentInfo prop to GameItem

--- src/components/GameItem/index.tsx
+++ src/components/GameItem/index.tsx
@@ -3,14 +3,28 @@ import { HiDownload } from 'react-icons/hi'
 
 import * as S from './styles'
 
+export type PaymentInfoProps = {
+  number: string
+  flag: string
+  img: string
+  purchaseDate: string
+}
+
 export type GameItemProps = {
   img: string
   title: string
   price: string
   downloadLink?: string
+  paymentInfo?: PaymentInfoProps
 }
 
-const GameItem = ({ img, title, price, downloadLink }: GameItemProps) => (
+const GameItem = ({
+  img,
+  title,
+  price,
+  downloadLink,
+  paymentInfo
+}: GameItemProps) => (
   <S.Wrapper>
     <S.GameContent>
       <S.ImageBox>
@@ -33,6 +47,18 @@ const GameItem = ({ img, title, price, downloadLink }: GameItemProps) => (
         <S.Price>{price}</S.Price>
       </S.Content>
     </S.GameContent>
+
+    {!!paymentInfo && (
+      <S.PaymentContent>
+        <p>{paymentInfo.purchaseDate}</p>
+        <S.CardInfo>
+          <span>{paymentInfo.number}</span>
+          <S.PaymentCardImageBox>
+            <Image src={paymentInfo.img} alt={paymentInfo.flag} fill />
+          </S.PaymentCardImageBox>
+        </S.CardInfo>
+      </S.PaymentContent>
+    )}
   </S.Wrapper>
 )
 
