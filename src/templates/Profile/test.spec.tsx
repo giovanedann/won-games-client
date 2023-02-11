a14cc1c1e5a36a351243b020389ed885a266d30b
Giovane <giovanedaniell@gmail.com>
2023-02-11 18:35:45 -0300

    test(templates): add test cases for Profile template

---
+++ src/templates/Profile/test.spec.tsx
@@ -0,0 +1,33 @@
+import { screen } from '@testing-library/react'
+import renderWithTheme from 'utils/tests/renderWithTheme'
+
+import Profile from '.'
+
+jest.mock('components/ProfileMenu', () => {
+  return {
+    __esModule: true,
+    default: function ProfileMenu() {
+      return <div data-testid="profile-menu-mock" />
+    }
+  }
+})
+
+describe('<Profile />', () => {
+  it('should render the right elements', () => {
+    renderWithTheme(
+      <Profile>
+        <h1>Profile template</h1>
+      </Profile>
+    )
+
+    expect(
+      screen.getByRole('heading', { name: /my profile/i, level: 2 })
+    ).toBeInTheDocument()
+
+    expect(screen.getByTestId('profile-menu-mock')).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /profile template/i, level: 1 })
+    ).toBeInTheDocument()
+  })
+})
