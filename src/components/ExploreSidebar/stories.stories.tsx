bc729e13410de11f30427998245d36f1fec305d3
Giovane <giovanedaniell@gmail.com>
2023-02-13 22:40:58 -0300

    feat(stories): add story for ExploreSidebar component

---
+++ src/components/ExploreSidebar/stories.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import ExploreSidebar from '.'
+import exploreSidebarMocks from './data.mock'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  padding: 1.6rem;
+  max-width: 32rem;
+`
+
+export default {
+  title: 'ExploreSidebar',
+  component: ExploreSidebar
+} as ComponentMeta<typeof ExploreSidebar>
+
+const Template: ComponentStory<typeof ExploreSidebar> = (args) => (
+  <Container>
+    <ExploreSidebar {...args} />
+  </Container>
+)
+
+export const Basic = Template.bind({})
+
+Basic.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'won-dark'
+  }
+}
+
+Basic.args = {
+  items: exploreSidebarMocks
+}
+
+export const WithInitialValues = Template.bind({})
+
+WithInitialValues.parameters = {
+  ...Basic.parameters
+}
+
+WithInitialValues.args = {
+  ...Basic.args,
+  initialValues: { windows: true, sort_by: 'low-to-high' }
+}
