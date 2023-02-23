c8881ce192bad6fccaf58ec35ab07fb712286c07
Giovane <giovanedaniell@gmail.com>
2023-02-23 18:08:16 -0300

    feat: populate games page with api data

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,15 +1,42 @@
 import exploreSidebarMocks from 'components/ExploreSidebar/data.mock'
-import gameCardSliderMock from 'components/GameCardSlider/data.mock'
+import { initializeApollo } from 'graphql/client'
+import { GET_GAMES } from 'graphql/queries/games'
 import GamesTemplate, { GameTemplateProps } from 'templates/Games'
+
+type GameDevelopers = { name: string }
+
+type GameProps = {
+  name: string
+  slug: string
+  cover: {
+    url: string
+  }
+  developers: GameDevelopers[]
+  price: number
+}
 
 export default function GamesPage(props: GameTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: GET_GAMES
+  })
+
   return {
     props: {
-      games: gameCardSliderMock,
+      games: data?.games?.map((game: GameProps) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover.url}`,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price)
+      })),
       filterItems: exploreSidebarMocks
     }
   }
