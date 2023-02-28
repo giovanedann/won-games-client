6aeb3925e00c29d4ddac8f2281e7c269f96c4c87
Giovane <giovanedaniell@gmail.com>
2023-02-27 23:37:48 -0300

    feat: add dynamic query on game slug page

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,57 +1,77 @@
-import galleryMock from 'components/Gallery/data.mock'
 import gameCardSliderMock from 'components/GameCardSlider/data.mock'
 import highlightMock from 'components/Highlight/data.mock'
+import { initializeApollo } from 'graphql/client'
+import {
+  GetGameBySlug,
+  GetGameBySlugVariables
+} from 'graphql/generated/GetGameBySlug'
+import { GetGames, GetGamesVariables } from 'graphql/generated/GetGames'
+import { GET_GAMES, GET_GAME_BY_SLUG } from 'graphql/queries/games'
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
 import Game, { GameTemplateProps } from 'templates/Game'
 
+const apolloClient = initializeApollo()
+
 export default function Index(props: GameTemplateProps) {
+  const router = useRouter()
+
+  // if the route is not generated yet, show a loading/skeleton component
+  if (router.isFallback) return null
+
   return <Game {...props} />
 }
 
 export async function getStaticPaths() {
-  return {
-    paths: [{ params: { slug: 'cyberpunk-2077' } }],
-    fallback: false
-  }
+  const { data } = await apolloClient.query<GetGames, GetGamesVariables>({
+    query: GET_GAMES,
+    variables: { limit: 9 }
+  })
+
+  const paths = data.games.map(({ slug }) => ({ params: { slug } }))
+
+  return { paths, fallback: true }
 }
 
-export async function getStaticProps() {
-  const descriptionHTML = `
-    <img src="https://items.gog.com/not_a_cp/ENG_product-page-addons-2020_yellow_on_black.png"><br>
-    * Exclusive Digital Comic - Cyberpunk 2077: Big City Dreams will be available in English only.
-    <hr><p class="module">Korean Voiceover will be added on 11th December 2020.</p><br><img alt="" src="https://items.gog.com/not_a_cp/EN/EN-About-the-Game.png"><br><br><b>Cyberpunk 2077</b> is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality. You can customize your character’s cyberware, skillset and playstyle, and explore a vast city where the choices you make shape the story and the world around you.
-    <br><br><img alt="" src="https://items.gog.com/not_a_cp/EN/EN-Mercenary-Outlaw.png"><br><br>
-    Become a cyberpunk, an urban mercenary equipped with cybernetic enhancements and build your legend on the streets of Night City.
-    <br><br><img alt="" src="https://items.gog.com/not_a_cp/EN/EN-City-of-the-Future.png"><br><br>
-    Enter the massive open world of Night City, a place that sets new standards in terms of visuals, complexity and depth.
-    <br><br><img alt="" src="https://items.gog.com/not_a_cp/EN/EN-Eternal-Life.png"><br><br>
-    Take the riskiest job of your life and go after a prototype implant that is the key to immortality.
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { data } = await apolloClient.query<
+    GetGameBySlug,
+    GetGameBySlugVariables
+  >({
+    query: GET_GAME_BY_SLUG,
+    variables: { slug: `${params?.slug}` }
+  })
 
+  if (!data.games.length) {
+    return { notFound: true }
+  }
 
-    <p class="description__copyrights">
-    CD PROJEKT®, Cyberpunk®, Cyberpunk 2077® are registered trademarks of CD PROJEKT S.A. © 2019
-    CD PROJEKT S.A. All rights reserved. All other copyrights and trademarks are the property of their
-    respective owners.
-  </p>`
+  const game = data.games[0]
 
   return {
     props: {
-      coverImg:
-        'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg',
+      revalidate: 60,
+      coverImg: `http://localhost:1337${game.cover?.src || ''}`,
       gameInfo: {
-        title: 'Cyberpunk 2077',
-        price: '59.00',
-        description:
-          'Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality'
+        title: game.name,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price),
+        description: game.short_description
       },
-      gallery: galleryMock,
-      description: descriptionHTML,
+      gallery: game.gallery.map(({ src, label }) => ({
+        src: `http://localhost:1337${src}`,
+        label
+      })),
+      description: game.description,
       details: {
-        developer: 'CD PROJEKT RED',
-        releaseDate: '2020-12-10T23:00:00',
-        platforms: ['windows'],
-        publisher: 'CD PROJEKT RED',
-        rating: 'BR18',
-        genres: ['Action', 'Role-playing']
+        developer: game.developers[0].name,
+        releaseDate: game.release_date,
+        platforms: game.platforms.map(({ name }) => name),
+        publisher: game.publisher?.name,
+        rating: game.rating,
+        genres: game.categories.map(({ name }) => name)
       },
       upcommingGames: gameCardSliderMock,
       upcommingHighlight: highlightMock,
