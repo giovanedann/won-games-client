60c5f3205c1c27f9fe10943fbee72574c5e199e6
Giovane <giovanedaniell@gmail.com>
2023-04-03 19:59:26 -0300

    chore: update storybook and project dependencies to latest versions

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "storybook-addon-next",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/nextjs",
+    options: {}
   },
   "staticDirs": ["../public"],
-  webpackFinal: (config) => {
-    config.resolve.modules.push(`${process.cwd()}/src`)
-    return config
+  webpackFinal: config => {
+    config.resolve.modules.push(`${process.cwd()}/src`);
+    return config;
   }
-}
+};
