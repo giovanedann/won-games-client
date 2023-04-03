95a878169e192c73c36538812e8204bd85e6b9fd
Giovane <giovanedaniell@gmail.com>
2023-04-03 20:58:10 -0300

    feat: add dynamic data on home page banners

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
-import bannersMock from 'components/BannerSlider/data.mock'
 import gamesMock from 'components/GameCardSlider/data.mock'
 import highlightMock from 'components/Highlight/data.mock'
+import { initializeApollo } from 'graphql/client'
+import { GET_HOME } from 'graphql/queries/home'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import BannerAdapter from 'adapters/banners.adapter'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: GET_HOME })
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 60,
+      banners: BannerAdapter(data.banners),
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
