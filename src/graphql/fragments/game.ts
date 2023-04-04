0558b9055832eec52ade8bf887cc6490fe462ca7
Giovane <giovanedaniell@gmail.com>
2023-04-03 23:39:58 -0300

    feat(graphql): add Game fragment

---
+++ src/graphql/fragments/game.ts
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client'
+
+export const GameFragment = gql`
+  fragment GameFragment on Game {
+    name
+    slug
+    cover {
+      url
+    }
+
+    developers {
+      name
+    }
+
+    price
+  }
+`
