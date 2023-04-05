9e46e65443c85e1385393e39c1677358faf06439
Giovane <giovanedaniell@gmail.com>
2023-04-04 21:59:00 -0300

    feat(adapters): create recommendedGames adapter

---
+++ src/adapters/recommended.adapter.ts
@@ -0,0 +1,26 @@
+import { QueryRecommended_recommended_section_games } from 'graphql/generated/QueryRecommended'
+import getImageUrl from 'utils/getImageUrl'
+
+export type Game = {
+  title: string
+  slug: string
+  developer: string
+  img: string
+  price: string
+}
+
+export function recommendedGamesAdapter(
+  apiNewGames: QueryRecommended_recommended_section_games[] | undefined
+): Game[] {
+  if (!apiNewGames) return []
+
+  return apiNewGames.map((game) => ({
+    title: game.name,
+    slug: game.slug,
+    developer: game.developers[0].name,
+    img: getImageUrl(
+      game.cover?.url ?? '/uploads/No_image_available_38adfae762.png'
+    ),
+    price: game.price === 0 ? 'Free' : `$${game.price}`
+  }))
+}
