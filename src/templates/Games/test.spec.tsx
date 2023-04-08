b4c74e90ebf602166e2868ed1959873dc77c8663
Giovane <giovanedaniell@gmail.com>
2023-04-07 21:26:24 -0300

    test(pages): ensure the right items are displayed on Games age after request succeeds

--- src/templates/Games/test.spec.tsx
+++ src/templates/Games/test.spec.tsx
@@ -4,6 +4,7 @@ import renderWithTheme from 'utils/tests/renderWithTheme'
 import { MockedProvider } from '@apollo/client/testing'
 
 import Games from '.'
+import { GET_GAMES } from 'graphql/queries/games'
 
 jest.mock('templates/Base', () => ({
   __esModule: true,
@@ -28,5 +29,50 @@ describe('<Games />', () => {
     )
 
     expect(screen.getByText(/loading\.../i)).toBeInTheDocument()
+  })
+
+  it('should render the sections correctly', async () => {
+    renderWithTheme(
+      <MockedProvider
+        mocks={[
+          {
+            request: { query: GET_GAMES, variables: { limit: 15 } },
+            result: {
+              data: {
+                games: [
+                  {
+                    name: 'Mocked game',
+                    slug: 'mocked-game',
+                    cover: { url: '/uploads/mocked-img-url.jpg' },
+                    developers: [{ name: 'Mocked developer' }],
+                    price: 65.99,
+                    __typename: 'Game'
+                  }
+                ]
+              }
+            }
+          }
+        ]}
+        addTypename={false}
+      >
+        <Games filterItems={exploreSidebarMocks} />
+      </MockedProvider>
+    )
+
+    // starts with loading until request is done
+    expect(screen.getByText(/loading\.../i)).toBeInTheDocument()
+
+    expect(
+      await screen.findByTestId('explore-sidebar-mock')
+    ).toBeInTheDocument()
+
+    expect(await screen.findByText(/mocked game/i)).toBeInTheDocument()
+
+    screen.debug()
+    expect(
+      screen.getByRole('button', {
+        name: /show more/i
+      })
+    ).toBeInTheDocument()
   })
 })
