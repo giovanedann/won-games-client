bf5f1ca67380e62bb8e9c7fe2810cd4541f8b930
Giovane <giovanedaniell@gmail.com>
2023-04-18 20:08:32 -0300

    test: ensure upcoming games adapter return the right object

--- src/adapters/upcoming-games.adapter.spec.ts
+++ src/adapters/upcoming-games.adapter.spec.ts
@@ -1,29 +1,29 @@
-import { QueryHome_newGames } from 'graphql/generated/QueryHome'
+import { QueryHome_upcomingGames } from 'graphql/generated/QueryHome'
 import { GameCardProps } from 'components/GameCard'
 import formatPrice from 'utils/formatPrice'
 import getImageUrl from 'utils/getImageUrl'
 import newGamesAdapter from './new-games.adapter'
 
-const apiGame: QueryHome_newGames = {
+const apiGame: QueryHome_upcomingGames = {
   __typename: 'Game',
   cover: {
     __typename: 'UploadFile',
     url: '/cover_url'
   },
   developers: [{ __typename: 'Developer', name: 'Rockstar Games' }],
-  name: 'New Game',
+  name: 'Upcoming Game',
   price: 300,
-  slug: 'new-game'
+  slug: 'upcoming-game'
 }
 
-describe('NewGamesAdapter', () => {
+describe('UpcomingGamesAdapter', () => {
   it('should return the right adapted object', () => {
     const expectedResult: GameCardProps = {
       developer: 'Rockstar Games',
       img: getImageUrl('/cover_url'),
       price: formatPrice(300),
-      slug: 'new-game',
-      title: 'New Game'
+      slug: 'upcoming-game',
+      title: 'Upcoming Game'
     }
 
     expect(newGamesAdapter([apiGame])).toStrictEqual([expectedResult])
