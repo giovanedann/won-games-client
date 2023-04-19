f6e2273b13589cf3cd8b8cd9b1ae053496669909
Giovane <giovanedaniell@gmail.com>
2023-04-19 17:42:24 -0300

    refactor(tests): adapt CartIcon tests to hooks

--- src/components/CartIcon/test.spec.tsx
+++ src/components/CartIcon/test.spec.tsx
@@ -1,5 +1,6 @@
 import { screen, render } from 'utils/tests/render'
 import CartIcon from '.'
+import { cartContextDefaultValues } from 'contexts/cart'
 
 describe('<CartIcon />', () => {
   it('should render the cart icon without quantity span if quantity is omitted or 0', () => {
@@ -12,7 +13,9 @@ describe('<CartIcon />', () => {
   })
 
   it('should render the cart item with quantity span if quantity is greater than 0', () => {
-    render(<CartIcon quantity={12} />)
+    render(<CartIcon />, {
+      cartProviderProps: { ...cartContextDefaultValues, itemsQuantity: 12 }
+    })
 
     expect(screen.getByLabelText(/shopping cart icon/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/cart items quantity/i)).toBeInTheDocument()
