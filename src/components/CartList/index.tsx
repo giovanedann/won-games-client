a2bb3ccb106daa7741510dd1156efc9972ef7628
Giovane <giovanedaniell@gmail.com>
2023-04-19 20:49:08 -0300

    feat(components): add dynamic data on CartList with Cart context

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,40 +1,43 @@
 import Button from 'components/Button'
 import Empty from 'components/Empty'
-import GameItem, { GameItemProps } from 'components/GameItem'
+import GameItem from 'components/GameItem'
 
 import * as S from './styles'
+import { useCart } from 'contexts/cart'
 
 export type CartListProps = {
-  items?: GameItemProps[]
-  total?: string
   hasButton?: boolean
 }
 
-const CartList = ({ items = [], total, hasButton = false }: CartListProps) => (
-  <S.Wrapper isEmpty={items.length === 0}>
-    {items.length > 0 && (
-      <>
-        {items.map((item) => (
-          <GameItem key={item.title} {...item} />
-        ))}
+const CartList = ({ hasButton = false }: CartListProps) => {
+  const { items, totalPrice } = useCart()
 
-        <S.Footer>
-          {!hasButton && <span>Total:</span>}
-          <S.Total>{total}</S.Total>
+  return (
+    <S.Wrapper isEmpty={items.length === 0}>
+      {items.length > 0 && (
+        <>
+          {items.map((item) => (
+            <GameItem key={item.title} {...item} />
+          ))}
 
-          {hasButton && <Button as="a">Buy it now</Button>}
-        </S.Footer>
-      </>
-    )}
+          <S.Footer>
+            {!hasButton && <span>Total:</span>}
+            <S.Total>{totalPrice}</S.Total>
 
-    {items.length === 0 && (
-      <Empty
-        title="Your cart is empty"
-        description="Go back to the store and explore great games and offers"
-        hasLink
-      />
-    )}
-  </S.Wrapper>
-)
+            {hasButton && <Button as="a">Buy it now</Button>}
+          </S.Footer>
+        </>
+      )}
+
+      {items.length === 0 && (
+        <Empty
+          title="Your cart is empty"
+          description="Go back to the store and explore great games and offers"
+          hasLink
+        />
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default CartList
