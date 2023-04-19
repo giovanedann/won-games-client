980db3aed9672233e8fd3c5123f1030e1f3f1431
Giovane <giovanedaniell@gmail.com>
2023-04-19 20:49:49 -0300

    test(components): add tests for new structure of CartList

--- src/components/CartList/test.spec.tsx
+++ src/components/CartList/test.spec.tsx
@@ -2,12 +2,41 @@ import { screen, render } from 'utils/tests/render'
 
 import CartList from '.'
 import cartListMock from './data.mock'
+import { cartContextDefaultValues } from 'contexts/cart'
 
 describe('<CartList />', () => {
   it('should render the cart list', () => {
-    render(<CartList items={cartListMock} total="R$ 330,00" />)
+    const cartProviderProps = {
+      ...cartContextDefaultValues,
+      items: cartListMock,
+      totalPrice: 'R$ 430,00'
+    }
+
+    render(<CartList />, {
+      cartProviderProps
+    })
 
     expect(screen.getAllByRole('heading')).toHaveLength(2)
-    expect(screen.getByText('R$ 330,00')).toHaveStyle({ color: '#F231A5' })
+    expect(screen.getByText('R$ 430,00')).toHaveStyle({ color: '#F231A5' })
+  })
+
+  it('should render the button', () => {
+    const cartProviderProps = {
+      ...cartContextDefaultValues,
+      items: cartListMock
+    }
+
+    render(<CartList hasButton />, {
+      cartProviderProps
+    })
+
+    expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
+  })
+
+  it('should render <Empty /> if there are no games in cart', () => {
+    render(<CartList hasButton />)
+
+    expect(screen.getByText(/your cart is empty/i)).toBeInTheDocument()
+    expect(screen.queryByText(/total/i)).not.toBeInTheDocument()
   })
 })
