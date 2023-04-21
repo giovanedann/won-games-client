aec798dbaeea0da547f99bcacd6e946fdb68ce39
Giovane <giovanedaniell@gmail.com>
2023-04-21 14:09:19 -0300

    fix: wrap app in CartProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,26 +5,29 @@ import theme from 'styles/theme'
 import { ThemeProvider } from 'styled-components'
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from 'infra/apollo/client'
+import { CartProvider } from 'contexts/cart'
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
 
   return (
     <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <title>Won Games</title>
-          <link rel="shortcut-icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="The game store that will surpass Steam, Epic, and every other store!"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <CartProvider>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <title>Won Games</title>
+            <link rel="shortcut-icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+              name="description"
+              content="The game store that will surpass Steam, Epic, and every other store!"
+            />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CartProvider>
     </ApolloProvider>
   )
 }
