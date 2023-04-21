fac7d21f09c030965797b68f62247d3219732254
Giovane <giovanedaniell@gmail.com>
2023-04-21 14:21:03 -0300

    fix: missing id on OrdersList mock

--- src/components/OrdersList/data.mock.ts
+++ src/components/OrdersList/data.mock.ts
@@ -1,5 +1,6 @@
 const ordersListMock = [
   {
+    id: '1',
     img: 'https://source.unsplash.com/user/willianjusten/151x70',
     title: 'Red Dead Redemption 2',
     price: 'R$ 215,00',
@@ -12,6 +13,7 @@ const ordersListMock = [
     }
   },
   {
+    id: '1',
     img: 'https://source.unsplash.com/user/willianjusten/151x70',
     title: 'Red Dead Redemption 2',
     price: 'R$ 215,00',
