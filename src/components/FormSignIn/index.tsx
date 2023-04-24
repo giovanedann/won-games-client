e6dc8836b4599bb8b890472de7238ff35fb94265
Giovane <giovanedaniell@gmail.com>
2023-04-24 15:55:49 -0300

    feat(components): add request on FormSignIn

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -5,27 +5,61 @@ import TextField from 'components/TextField'
 import Link from 'next/link'
 import { MdLockOutline, MdOutlineMail } from 'react-icons/md'
 import * as S from './styles'
+import { FormEvent, useState } from 'react'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+type SignInData = { email: string; password: string }
 
 function FormSignIn() {
+  const [signInFormValues, setSignInFormValues] = useState<SignInData>({
+    email: '',
+    password: ''
+  })
+  const { push } = useRouter()
+
+  function handleInputChange(field: keyof SignInData, value: string) {
+    setSignInFormValues((prev) => ({
+      ...prev,
+      [field]: value
+    }))
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const result = await signIn('credentials', {
+      ...signInFormValues,
+      redirect: false,
+      callbackUrl: '/'
+    })
+
+    if (result?.url) {
+      return push(result?.url as string)
+    }
+  }
+
   return (
     <FormWrapper>
-      <S.Form>
+      <S.Form onSubmit={handleSubmit}>
         <TextField
           name="email"
           placeholder="E-mail"
           type="email"
           icon={<MdOutlineMail />}
+          onInputChange={(value) => handleInputChange('email', value)}
         />
         <TextField
           name="password"
           placeholder="Password"
           type="password"
           icon={<MdLockOutline />}
+          onInputChange={(value) => handleInputChange('password', value)}
         />
 
         <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
 
-        <Button size="large" fullWidth>
+        <Button type="submit" size="large" fullWidth>
           Sign In
         </Button>
 
