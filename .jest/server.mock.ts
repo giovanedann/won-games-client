f34617d83adf89645d23caafaabd089afb3cae5e
Giovane <giovanedaniell@gmail.com>
2023-05-01 13:50:54 -0300

    chore(deps-dev): downgrade node-fetch to version 2^ to prevent errors

--- .jest/server.mock.ts
+++ .jest/server.mock.ts
@@ -1,5 +1,8 @@
-global.fetch = require('node-fetch')
 import { server } from '../src/utils/tests/server'
+
+global.fetch = require('node-fetch')
+global.Request = require('node-fetch').Request
+global.Response = require('node-fetch').Response
 
 beforeAll(() => {
   server.listen()
