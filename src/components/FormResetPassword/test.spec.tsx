a0eda5a8a409d93aedea35e9a9d74877e04f1a85
Giovane <giovanedaniell@gmail.com>
2023-05-01 14:13:29 -0300

    test(components): ensure FormResetPassword show validation errors

--- src/components/FormResetPassword/test.spec.tsx
+++ src/components/FormResetPassword/test.spec.tsx
@@ -2,6 +2,7 @@
 import { render, screen } from 'utils/tests/render'
 
 import FormResetPassword from '.'
+import userEvent from '@testing-library/user-event'
 
 const useRouter = jest.spyOn(require('next/router'), 'useRouter')
 const query = {}
@@ -15,5 +16,19 @@ describe('<FormResetPassword />', () => {
     expect(screen.getByPlaceholderText(/^password$/i)).toBeInTheDocument()
     expect(screen.getByPlaceholderText(/confirm password/i)).toBeInTheDocument()
     expect(screen.getByRole('button', { name: /reset/i })).toBeInTheDocument()
+  })
+
+  it('should show the validation errors', async () => {
+    const user = userEvent.setup()
+    render(<FormResetPassword />)
+
+    expect(
+      screen.getByText(/"password" is not allowed to be empty/i)
+    ).toBeInTheDocument()
+
+    await user.type(screen.getByPlaceholderText(/^password$/i), '123')
+    await user.type(screen.getByPlaceholderText(/confirm password/i), '133')
+
+    expect(screen.getByText(/Passwords do not match/i)).toBeInTheDocument()
   })
 })
