0a17d8435c6b8737c8761c6bf6a804f50121afc7
Giovane <giovanedaniell@gmail.com>
2023-05-01 13:51:58 -0300

    feat(utils): add msw handler for forgot-password route

--- src/utils/tests/handlers.ts
+++ src/utils/tests/handlers.ts
@@ -1,4 +1,31 @@
-import { rest } from 'msw' // eslint-disable-line
+import { rest } from 'msw'
+
+type LoginRequestBody = {
+  email: string
+}
 
 // interceptors for the mocked server of server.ts file
-export const handlers = []
+export const handlers = [
+  rest.post<LoginRequestBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+    async (request, response, context) => {
+      const { email } = await request.json<LoginRequestBody>()
+
+      // error case
+      if (email === 'false@email.com') {
+        return response(
+          context.status(400),
+          context.json({
+            error: 'Bad request',
+            message: {
+              messages: [{ message: 'This email does not exist' }]
+            }
+          })
+        )
+      }
+
+      // success case
+      return response(context.status(200), context.json({ ok: true }))
+    }
+  )
+]
