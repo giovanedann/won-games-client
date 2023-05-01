c3e81de2a0abd85870c4b707c32a578be14088ed
Giovane <giovanedaniell@gmail.com>
2023-05-01 11:11:47 -0300

    feat: add reset-password flow

--- src/components/FormResetPassword/index.tsx
+++ src/components/FormResetPassword/index.tsx
@@ -1,17 +1,23 @@
-import { MdLockOutline } from 'react-icons/md'
+import { MdErrorOutline, MdLockOutline } from 'react-icons/md'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
 
-import { FormWrapper } from 'components/Form'
+import { FormError, FormLoader, FormWrapper } from 'components/Form'
 import { FormEvent, useState } from 'react'
 import { ResetPasswordData, resetPasswordValidation } from 'validators/forms'
+import { useRouter } from 'next/router'
+import { signIn } from 'next-auth/react'
 
 function FormResetPassword() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [formError, setFormError] = useState('')
   const [resetPasswordValues, setResetPasswordValues] =
     useState<ResetPasswordData>({
       password: '',
       password_confirmation: ''
     })
+
+  const { query } = useRouter()
 
   function handleInputChange(field: keyof ResetPasswordData, value: string) {
     setResetPasswordValues((prev) => ({
@@ -22,6 +28,40 @@ function FormResetPassword() {
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault()
+    setIsLoading(true)
+
+    const body = {
+      password: resetPasswordValues.password,
+      passwordConfirmation: resetPasswordValues.password_confirmation,
+      code: query.code
+    }
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      }
+    )
+
+    const data = await response.json()
+
+    if (data.error) {
+      const { message } = data
+      const [errorMessage] = message.messages
+
+      setFormError(errorMessage.message)
+      setIsLoading(false)
+    } else {
+      signIn('credentials', {
+        email: data.user.email,
+        password: resetPasswordValues.password,
+        callbackUrl: '/'
+      })
+    }
   }
 
   const formErrors = resetPasswordValidation(resetPasswordValues)
@@ -29,6 +69,12 @@ function FormResetPassword() {
 
   return (
     <FormWrapper>
+      {formError && (
+        <FormError>
+          <MdErrorOutline /> {formError}
+        </FormError>
+      )}
+
       <form onSubmit={handleSubmit}>
         <TextField
           name="password"
@@ -49,8 +95,13 @@ function FormResetPassword() {
           error={formErrors['password_confirmation']}
         />
 
-        <Button size="large" type="submit" fullWidth disabled={!isFormValid}>
-          {'Reset'}
+        <Button
+          size="large"
+          type="submit"
+          fullWidth
+          disabled={!isFormValid || isLoading}
+        >
+          {isLoading ? <FormLoader /> : 'Reset'}
         </Button>
       </form>
     </FormWrapper>
