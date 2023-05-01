42de805c5322b5ecd31931da016f7cf66170d35a
Giovane <giovanedaniell@gmail.com>
2023-05-01 11:57:42 -0300

    chore: config env variables for jest

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,7 +1,12 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+import dotenv from 'dotenv'
 
 global.console = {
   ...console,
   error: jest.fn()
 }
+
+dotenv.config({
+  path: '.env.development'
+})
