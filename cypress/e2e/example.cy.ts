6cced49bfc3b41ed8c0ae6b70db11042f947beea
Giovane <giovanedaniell@gmail.com>
2023-05-18 00:14:13 -0300

    chore(e2e): add testing library config to cypress

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,5 +1,11 @@
 describe('Test', () => {
   it('should visit google', () => {
     cy.google()
+
+    cy.findByRole('combobox').type('fotos de cachorros').wait(500).type('{esc}')
+
+    cy.findByRole('button', { name: /pesquisa google/i }).click()
+
+    cy.findByText(/imagens de cachorros/i).should('exist')
   })
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,1 +1,3 @@
+import '@testing-library/cypress/add-commands'
+
 Cypress.Commands.add('google', () => cy.visit('https://google.com'))
