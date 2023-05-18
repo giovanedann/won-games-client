118698950ff08a0b44d470706daf3bdb6ee50ec2
Giovane <giovanedaniell@gmail.com>
2023-05-18 12:19:15 -0300

    test(e2e): ensure home page renders the banner correctly

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,15 @@
 describe('Home page', () => {
-  it('should go to home page', () => {
+  it('should render the home sections', () => {
+    // visit the home page
     cy.visit('/')
+
+    // find the slick-slider class (banner component) and find the correct items
+    cy.get('.slick-slider')
+      .first()
+      .within(() => {
+        cy.findByRole('heading', { name: /god of war - ragnarok/i })
+        cy.findByRole('link', { name: /buy now/i })
+        cy.findByText(/best seller/i)
+      })
   })
 })
