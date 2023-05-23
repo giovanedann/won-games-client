f56b7af1794e20be923a284c3cfd4b176232fb62
Giovane <giovanedaniell@gmail.com>
2023-05-22 22:34:09 -0300

    test(e2e): ensure sign in redirects to the previous protected route

--- cypress/e2e/sign-in.cy.ts
+++ cypress/e2e/sign-in.cy.ts
@@ -13,4 +13,23 @@ describe('SignIn workflow', () => {
     cy.findByRole('link', { name: /sign in/i }).should('exist')
     cy.findByText(/cypress/i).should('not.exist')
   })
+
+  it('should redirect to the previous page url after sign in succeeds', () => {
+    cy.visit('/profile/me')
+
+    cy.location('href').should(
+      'eq',
+      `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/me`
+    )
+
+    cy.signIn()
+
+    cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/me`)
+
+    cy.findByRole('button', { name: /save changes/i }).should('exist')
+    cy.findByRole('button', { name: /reset password/i }).should('exist')
+
+    cy.findByLabelText(/username/i).should('have.value', 'Cypress')
+    cy.findByLabelText(/e-mail/i).should('have.value', 'e2e@wongames.com')
+  })
 })
